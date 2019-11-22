249fd1537f57db8b7febaa388faf54ffb73b7729
Sushil Mishra <mishrasushil889@gmail.com>
2019-11-22 18:03:02 +0530

    assign letter to player either X or 0

--- Tic_Tac_Toe.php
+++ Tic_Tac_Toe.php
@@ -8,4 +8,18 @@ for($row=1;$row<=pow($row_of_board,2);$row++)
    $board_array[$row]="";
 }
 
+//assign letter to player
+echo "Press enter to play game";
+fscanf(STDIN,"%s");
+$random_letter = rand(0,1);
+if($random_letter==0)
+{
+   $player_letter = "X";
+}
+else
+{
+   $player_letter = "0";
+}
+echo "Letter ".$player_letter." assigned to player\n";
+
 ?>
